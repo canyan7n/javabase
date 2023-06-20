12242f50cf631161aa83e786bd243b825a954291
macheng <1752520757@qq.com>
2023-06-20 15:16:43 +0800

    commit on idea in ConnectToMySQL

--- src/main/java/com/canyan7n/jb/jdbc/ConnectToMySQL.java
+++ src/main/java/com/canyan7n/jb/jdbc/ConnectToMySQL.java
@@ -1,7 +1,26 @@
 package com.canyan7n.jb.jdbc;
 
-public class ConnectToMySQL {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+public class ConnectToMySQL {
+    public static void main(String[] args) throws Exception {
+        //加载数据库连接驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String URL = "jdbc:mysql://localhost:3306/study?serverTimezone=UTC";
+        String username = "root";
+        String password = "root";
+        //创建连接
+        Connection connection = DriverManager.getConnection(URL, username, password);
+        //执行sql
+        String sql = "select * from t_user";
+        Statement statement = connection.createStatement();
+        boolean execute = statement.execute(sql);
+        if (execute){
+            System.out.println("查询成功");
+        }else {
+            System.out.println("查询失败");
+        }
     }
 }
