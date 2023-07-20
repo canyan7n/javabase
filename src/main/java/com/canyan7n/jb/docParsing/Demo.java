afaba52b87239ff94e1b95100c359ba011be8233
macheng <1752520757@qq.com>
2023-07-20 16:21:31 +0800

    commit on idea in excel parsing

---
+++ src/main/java/com/canyan7n/jb/docParsing/Demo.java
@@ -0,0 +1,23 @@
+package com.canyan7n.jb.docParsing;
+
+import org.apache.poi.util.StringUtil;
+
+/**
+ * @projectName: javabase
+ * @package: com.canyan7n.jb.docParsing
+ * @className: Demo
+ * @author: 马成
+ * @description: TODO
+ * @date: 2023/7/20 15:12
+ * @version: 1.0
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        String s = "";
+        for (int i = 0; i < 26; i++) {
+//            Character char = 'a' + i;
+        }
+        System.out.println(s);
+    }
+}
--- src/main/java/com/canyan7n/jb/docParsing/DocParsing.java
+++ src/main/java/com/canyan7n/jb/docParsing/DocParsing.java
@@ -1,11 +1,15 @@
 package com.canyan7n.jb.docParsing;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.List;
 
 /**
@@ -20,15 +24,45 @@ import java.util.List;
 
 public class DocParsing {
     public static void main(String[] args) throws Exception{
-        word();
+        //word();
+        excel();
     }
     public static void word()throws Exception{
         FileInputStream fileInputStream = new FileInputStream(new File("1.docx"));
-        XWPFDocument xwpfDocument = new XWPFDocument(fileInputStream);
-        List<XWPFParagraph> paragraph = xwpfDocument.getParagraphs();
-        for (XWPFParagraph xwpfParagraph :paragraph){
-            String text = xwpfParagraph.getText();
+        XWPFDocument document = new XWPFDocument(fileInputStream);
+        List<XWPFParagraph> paragraphs = document.getParagraphs();
+        for (XWPFParagraph paragraph :paragraphs){
+            String text = paragraph.getText();
             System.out.println(text);
         }
+        fileInputStream.close();
+    }
+    public static void excel()throws Exception{
+        FileInputStream fileInputStream = new FileInputStream(new File("1.xlsx"));
+        Workbook sheets = new XSSFWorkbook(fileInputStream);
+        Sheet sheet = sheets.getSheetAt(0);
+
+        for (Row row: sheet){
+            for (Cell cell : row){
+                String cellValue = "";
+                switch (cell.getCellType()){
+                    case STRING:
+                        cellValue = cell.getStringCellValue();
+                        break;
+                    case NUMERIC:
+                        cellValue = String.valueOf(cell.getNumericCellValue());
+                        break;
+                    case BOOLEAN:
+                        cellValue = String.valueOf(cell.getBooleanCellValue());
+                        break;
+                    default:
+                        break;
+                }
+                System.out.print(cellValue + "\t|");
+            }
+            System.out.println();
+        }
+        sheets.close();
+        fileInputStream.close();
     }
 }
